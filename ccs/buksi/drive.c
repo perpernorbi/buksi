76dd8bfbd2787b7718a6eefbc6b9521c0e203f83
KOSZÓ, Norbert <k.norbi@gmail.com>
2017-03-31 21:58:21 +0200

    Measuring speed is working

--- ccs/buksi/drive.c
+++ ccs/buksi/drive.c
@@ -9,6 +9,7 @@
 #include <stddef.h>
 
 #include "serial.h"
+#include "tick.h"
 
 #define WHEEL_COUNT 2
 static volatile unsigned char const * const wheel_encoder_port[WHEEL_COUNT] = { &P2IN, &P2IN };
@@ -18,6 +19,12 @@ static volatile unsigned int * const wheel_drive_pwm_reg[WHEEL_COUNT] = { &TA1CC
 static const char wheel_drive_direction_pin[WHEEL_COUNT] = { BIT2, BIT3 };
 static unsigned char wheel_encoder_buffer[WHEEL_COUNT] = { 0, 0 };
 static unsigned char wheel_encoder_status[WHEEL_COUNT] = { 0, 0 };
+
+static int wheel_position[WHEEL_COUNT] = { 0, 0 };
+static int wheel_speed_last_position[WHEEL_COUNT] = { 0, 0 };
+static int wheel_measured_spped_reciprocal[WHEEL_COUNT] = { 0, 0 };
+
+static const unsigned int speed_update_interval_mask = 0x3FF;
 
 void drive_setVelocity(const char velocities[2])
 {
@@ -34,14 +41,24 @@ void drive_setVelocity(const char velocities[2])
 	}
 }
 
+static void update_measured_speed(unsigned char wheel_id)
+{
+	wheel_measured_spped_reciprocal[wheel_id] = wheel_position[wheel_id] - wheel_speed_last_position[wheel_id];
+	wheel_speed_last_position[wheel_id] = wheel_position[wheel_id];
+	serial_sendChar((char)(wheel_measured_spped_reciprocal[wheel_id] & 0xFF));
+	serial_sendChar((char)(wheel_measured_spped_reciprocal[wheel_id] >> 8));
+}
+
 static void encode(size_t wheel_id)
 {
 	if ((*wheel_encoder_port[wheel_id]) & wheel_encoder_pin[wheel_id]) wheel_encoder_buffer[wheel_id] |= 0x01;
 	if ((wheel_encoder_status[wheel_id] == 1) && ((wheel_encoder_buffer[wheel_id] & 0x0F) == 0x00)) {
 		wheel_encoder_status[wheel_id] = 0;
+		++wheel_position[wheel_id];
 	}
 	if ((wheel_encoder_status[wheel_id] == 0) && (wheel_encoder_buffer[wheel_id] & 0x0F == 0x0F)) {
 		wheel_encoder_status[wheel_id] = 1;
+		++wheel_position[wheel_id];
 	}
 	wheel_encoder_buffer[wheel_id] <<= 1;
 
@@ -51,4 +68,8 @@ void drive_tick()
 {
 	encode(0);
 	//encode(1);
+	if (! (tick_counter & speed_update_interval_mask)) {
+		update_measured_speed(0);
+		//update_measured_speed(1);
+	}
 }
