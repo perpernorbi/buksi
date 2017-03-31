2f213fbc72f1e985cc2d2b5205d885b0d5807a90
KOSZÓ, Norbert <k.norbi@gmail.com>
2017-03-31 21:32:51 +0200

    Remove clutter

--- ccs/buksi/main.c
+++ ccs/buksi/main.c
@@ -6,31 +6,17 @@
 
 int main(void)
 {
-	unsigned tick_counter = 0;
 	Grace_init();
 	serial_initialize();
     while (1) {
     	__bis_SR_register(CPUOFF + GIE);
     	if (tick_get_and_clear()) {
-//    		++i;
-//    		if (i == 1000) {
-//    		    while (!(IFG2 & UCA0TXIFG)); // Poll TXIFG to until set
-//    		    UCA0TXBUF = '.';       // TX -> RXed character
-//    		    i = 0;
-//    		}
+        	drive_tick();
+
     		const char * dataframe = serial_getNextFrame();
     		if (dataframe)
     			drive_setVelocity(dataframe+1);
     	}
-    	drive_tick(tick_counter);
-/*    	if (P2IN & BIT5) wheel |= 0x01;
-    	if (!(i & 0x03)) {
-    	    while (!(IFG2 & UCA0TXIFG)); // Poll TXIFG to until set
-    	    UCA0TXBUF = wheel;
-    	}
-    	wheel <<= 1;
-    	++i;*/
-
     }
     
     return (0);
