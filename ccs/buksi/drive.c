d120b12ec6881fb07684fdfe3ea72e783d03ed19
KOSZÓ, Norbert <k.norbi@gmail.com>
2017-04-01 08:58:58 +0200

    Preparation for the speed controller

--- ccs/buksi/drive.c
+++ ccs/buksi/drive.c
@@ -15,53 +15,69 @@
 static volatile unsigned char const * const wheel_encoder_port[WHEEL_COUNT] = { &P2IN, &P2IN };
 static const char wheel_encoder_pin[WHEEL_COUNT] = { BIT5, BIT0 };
 static volatile unsigned char * const wheel_drive_port[WHEEL_COUNT] = { &P2OUT, &P2OUT };
-static volatile unsigned int * const wheel_drive_pwm_reg[WHEEL_COUNT] = { &TA1CCR1, &TA1CCR2 };
-static const char wheel_drive_direction_pin[WHEEL_COUNT] = { BIT2, BIT3 };
+static volatile unsigned int * const wheel_drive_pwm_reg[WHEEL_COUNT] = { &TA1CCR2, &TA1CCR1 };
+static const char wheel_drive_direction_pin[WHEEL_COUNT] = { BIT3, BIT2 };
 static unsigned char wheel_encoder_buffer[WHEEL_COUNT] = { 0, 0 };
 static unsigned char wheel_encoder_status[WHEEL_COUNT] = { 0, 0 };
 
 static int wheel_position[WHEEL_COUNT] = { 0, 0 };
 static int wheel_velocity_last_position[WHEEL_COUNT] = { 0, 0 };
-static int wheel_measured_velocity_reciprocal[WHEEL_COUNT] = { 0, 0 };
+static signed char wheel_measured_velocity[WHEEL_COUNT] = { 0, 0 };
+static signed char wheel_desired_velocity[WHEEL_COUNT] = { 0, 0 };
 
 static const unsigned int speed_update_interval_mask = 0x1FF;
 
 void drive_setVelocity(const char velocities[2])
 {
 	unsigned char i;
-	for (i = 0; i < 2; ++i) {
-		char velocity = velocities[i];
-		if (velocity > 0x80) {
-			(*wheel_drive_port[i]) |= wheel_drive_direction_pin[i];
-			velocity = 0xFF - velocity;
-		} else {
-			(*wheel_drive_port[i]) &= ~wheel_drive_direction_pin[i];
-		}
-		(*wheel_drive_pwm_reg[i]) = (velocity & 0x7F) << 8;
-	}
+	for (i = 0; i < 2; ++i)
+		wheel_desired_velocity[i] = velocities[i];
+		//Dimension conversion must be done here, if necessary
 }
 
 static void update_measured_velocity(unsigned char wheel_id)
 {
-	wheel_measured_velocity_reciprocal[wheel_id] = wheel_position[wheel_id] - wheel_velocity_last_position[wheel_id];
+	wheel_measured_velocity[wheel_id] = wheel_position[wheel_id] - wheel_velocity_last_position[wheel_id];
 	wheel_velocity_last_position[wheel_id] = wheel_position[wheel_id];
-	serial_sendChar((char)(wheel_measured_velocity_reciprocal[wheel_id] & 0xFF));
-	serial_sendChar((char)(wheel_measured_velocity_reciprocal[wheel_id] >> 8));
+	serial_sendChar(wheel_measured_velocity[wheel_id]);
 }
 
-static void encode(size_t wheel_id)
+static void encode(unsigned char wheel_id)
 {
 	if ((*wheel_encoder_port[wheel_id]) & wheel_encoder_pin[wheel_id]) wheel_encoder_buffer[wheel_id] |= 0x01;
 	if ((wheel_encoder_status[wheel_id] == 1) && ((wheel_encoder_buffer[wheel_id] & 0x0F) == 0x00)) {
 		wheel_encoder_status[wheel_id] = 0;
-		++wheel_position[wheel_id];
+		if (wheel_desired_velocity[wheel_id] >= 0)
+			++wheel_position[wheel_id];
+		else
+			--wheel_position[wheel_id];
 	}
 	if ((wheel_encoder_status[wheel_id] == 0) && (wheel_encoder_buffer[wheel_id] & 0x0F == 0x0F)) {
 		wheel_encoder_status[wheel_id] = 1;
-		++wheel_position[wheel_id];
+		if (wheel_desired_velocity[wheel_id] >= 0)
+			++wheel_position[wheel_id];
+		else
+			--wheel_position[wheel_id];
 	}
 	wheel_encoder_buffer[wheel_id] <<= 1;
 
+}
+
+static void update_pwm(unsigned char wheel, char pwm)
+{
+	if (pwm & 0x80) {
+		(*wheel_drive_port[wheel]) |= wheel_drive_direction_pin[wheel];
+        pwm &= 0x7F;
+    } else {
+    	(*wheel_drive_port[wheel]) &= ~wheel_drive_direction_pin[wheel];
+    }
+	(*wheel_drive_pwm_reg[wheel]) = pwm << 8;
+}
+
+static void control_pwm(unsigned char wheel)
+{
+	//int error = wheel_measured_velocity[0] - wheel_desired_velocity[0];
+	update_pwm(wheel, wheel_desired_velocity[wheel]);
 }
 
 void drive_tick()
@@ -71,5 +87,6 @@ void drive_tick()
 	if (! (tick_counter & speed_update_interval_mask)) {
 		update_measured_velocity(0);
 		//update_measured_speed(1);
+		control_pwm(0);
 	}
 }
